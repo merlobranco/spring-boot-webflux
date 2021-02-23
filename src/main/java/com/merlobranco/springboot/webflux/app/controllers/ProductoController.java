fac4b547ddd5395a8af5b95802e6cebacbb5b946
Brais Cidras <braisomary@gmail.com>
2021-02-23 21:26:35 +0200

    Chunked Mode View Names

--- src/main/java/com/merlobranco/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/merlobranco/springboot/webflux/app/controllers/ProductoController.java
@@ -71,6 +71,20 @@ public class ProductoController {
 		return "listar";
 	}
 	
+	@GetMapping("/listar-chunked")
+	public String listarChunked(Model model) {
+		Flux<Producto> productos = dao.findAll().map(producto-> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).repeat(5000);
+		
+		
+		model.addAttribute("titulo", "Listado de productos");
+		model.addAttribute("productos", productos);
+		
+		return "listar-chunked";
+	}
+	
 	
 
 }
