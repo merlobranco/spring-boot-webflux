edb876720c7477c79a077ebf55e49ee7811bc3cd
Brais Cidras <braisomary@gmail.com>
2021-02-23 20:42:16 +0200

    Reactive Data Driven Mode for handling Backpressure

--- src/main/java/com/merlobranco/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/merlobranco/springboot/webflux/app/controllers/ProductoController.java
@@ -1,11 +1,14 @@
 package com.merlobranco.springboot.webflux.app.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.merlobranco.springboot.webflux.app.models.dao.ProductoDao;
 import com.merlobranco.springboot.webflux.app.models.documents.Producto;
@@ -34,6 +37,25 @@ public class ProductoController {
 		
 		model.addAttribute("titulo", "Listado de productos");
 		model.addAttribute("productos", productos);
+		
+		return "listar";
+	}
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		
+		// There is no need for subscribing, Thymeleaf will do it for us
+		// The thymeleaf template is the observer subscribed to the observable
+		Flux<Producto> productos = dao.findAll().map(producto-> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(p -> log.info(p.getNombre()));
+		
+		model.addAttribute("titulo", "Listado de productos");
+		// We don´t have to wait until all the flow is returned or solved
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 1));
 		
 		return "listar";
 	}
